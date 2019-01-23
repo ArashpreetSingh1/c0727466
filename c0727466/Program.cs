2dea4a704999fdf093520016419b026ecd41a900
Arashpreet Singh <c0727466@mylambton.ca>
2019-01-23 11:56:18 -0500

    Class project

--- c0727466/Program.cs
+++ c0727466/Program.cs
@@ -10,6 +10,20 @@ namespace c0727466
     {
         static void Main(string[] args)
         {
-        }
+            int sum = 0;
+            int i = 1;
+
+            while (i <= 5)
+            {
+                sum += i;
+                i++;
+            }
+            Console.WriteLine("The sum of {0} ", sum);
+            Console.ReadLine();
+
+
+
+
+       } 
     }
 }
